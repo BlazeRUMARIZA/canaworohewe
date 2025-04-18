782c8ca16ec1f10760cfd906267ba9ec4c401a04
BlazeRUMARIZA <blazerumariza2004@gmail.com>
2025-04-18 16:37:42 +0300

    Charcoal & Gas
    Academic project of management of gas & charcoal

---
+++ src/main/java/com/canaworohewe/CanaworoheweApplication.java
@@ -0,0 +1,13 @@
+package com.canaworohewe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CanaworoheweApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CanaworoheweApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/canaworohewe/CanaworoheweApplicationTests.java
@@ -0,0 +1,13 @@
+package com.canaworohewe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CanaworoheweApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
